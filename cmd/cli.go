bd161736bc184fc82f3126af7a0b60a03c7586ca
Lukas Baron <lukas.baron@tngtech.com>
2025-01-05 00:16:44 +0100

    Cli shows solar times (value still incorrect though)

--- cmd/cli.go
+++ cmd/cli.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"math"
 	"net/http"
 	"time"
 )
@@ -53,9 +54,11 @@ func getIPInfo(debug bool) (*IPInfo, error) {
 	if debug {
 		fmt.Println(string(body)) // Print the response body
 	}
-
 	var ipInfo IPInfo
 	err = json.Unmarshal(body, &ipInfo)
+	if debug {
+		fmt.Printf("IPInfo: %+v\n", ipInfo) // Debug print IPInfo struct
+	}
 	if err != nil {
 		var errorResponse map[string]interface{}
 		err = json.Unmarshal(body, &errorResponse)
@@ -71,6 +74,26 @@ func getIPInfo(debug bool) (*IPInfo, error) {
 	return &ipInfo, nil
 }
 
+// Declination calculates the solar declination angle in degrees for a given day of the year.
+func Declination(dayOfYear int) float64 {
+	L := (2 * math.Pi / 365.25) * float64(dayOfYear-81)
+	declination := 23.45 * math.Sin(L)
+	return declination
+}
+
+func equationOfTime(dayOfYear int) float64 {
+	L := (2 * math.Pi / 365.25) * float64(dayOfYear-81)
+	return (229.18*math.Sin(2*L) - 7.53*math.Sin(4*L) + 2.89*math.Sin(6*L)) / 60.0
+}
+
+func meanSolarTime(standardTime time.Time, equationOfTime float64) time.Time {
+	return standardTime.Add(time.Duration(equationOfTime * float64(time.Hour)))
+}
+
+func apparentSolarTime(meanSolarTime time.Time, declination float64) time.Time {
+	return meanSolarTime.Add(time.Duration(4.0 * math.Sin(declination) * float64(time.Minute)))
+}
+
 func main() {
 	debug := flag.Bool("debug", false, "print JSON response")
 	flag.Parse()
@@ -81,8 +104,35 @@ func main() {
 		return
 	}
 
-fmt.Printf("Current Time (UTC): %s\n", time.Now().UTC().Format("2006-01-02 15:04:05"))
-fmt.Printf("Location:           %s, %s, %s\n", ipInfo.City, ipInfo.Region, ipInfo.Country)
-fmt.Printf("Coordinates:        %.4f°N, %.4f°E\n", ipInfo.Latitude, ipInfo.Longitude)
-fmt.Printf("Timezone:           %s (UTC%s)\n", ipInfo.Timezone, ipInfo.UTCOffset)
+	// Calculate standard time based on timezone
+	utcTime := time.Now().UTC()
+	location, err := time.LoadLocation(ipInfo.Timezone)
+	if err != nil {
+		fmt.Println("Error loading timezone:", err)
+		return
+	}
+	standardTime := utcTime.In(location)
+
+	dayOfYear := standardTime.YearDay()
+
+	eot := equationOfTime(dayOfYear)
+	meanSolarTime := meanSolarTime(standardTime, eot)
+
+	declination := Declination(dayOfYear)
+	apparentSolarTime := apparentSolarTime(meanSolarTime, declination)
+
+	fmt.Printf("Location:           %s, %s, %s\n", ipInfo.City, ipInfo.Region, ipInfo.Country)
+	fmt.Printf("Coordinates:        %.4f°N, %.4f°E\n", ipInfo.Latitude, ipInfo.Longitude)
+	fmt.Printf("Timezone:           %s (UTC%s)\n", ipInfo.Timezone, ipInfo.UTCOffset)
+
+	fmt.Printf("UTC:                %s\n", time.Now().UTC().Format("15:04:05 / 2006-01-02"))
+	fmt.Printf("Local:              %s\n", standardTime.Format("15:04:05"))
+
+	fmt.Printf("\nCorrections:\n")
+	fmt.Printf("Equation of time:   %.1f minutes\n", eot)
+
+	fmt.Printf("\nTime Calculations:\n")
+	fmt.Printf("Mean solar time:    %s\n", meanSolarTime.Format("15:04:05"))
+	fmt.Printf("Apparent solar time:%s\n", apparentSolarTime.Format("15:04:05"))
+
 }
